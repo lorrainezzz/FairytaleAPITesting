d0422f11c86055c14b7faa2d373070239963a106
Yang Zou <zoulorraine@gmail.com>
2019-10-13 23:43:13 +0100

    Reset datastore to guarantee test isolation

--- test/functional/api/donationsTest.js
+++ test/functional/api/donationsTest.js
@@ -7,6 +7,25 @@ const _ = require("lodash");
 let datastore = require("../../../models/donations");
 
 describe("Donations", () => {
+
+    beforeEach(() => {
+        while (datastore.length > 0) {
+            datastore.pop();
+        }
+        datastore.push({
+            id: 1000000,
+            paymenttype: "PayPal",
+            amount: 1600,
+            upvotes: 1
+        });
+        datastore.push({
+            id: 1000001,
+            paymenttype: "Direct",
+            amount: 1100,
+            upvotes: 2
+        });
+    });
+
     describe("GET /donations", () => {
         it("should return all the donations", done => {
             request(server)
