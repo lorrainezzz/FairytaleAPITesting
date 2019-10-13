3581f78f9e2ab7c210daf681c7303f29f7ea99e7
Yang Zou <zoulorraine@gmail.com>
2019-10-13 23:26:56 +0100

    Get all donations endpoint tested

--- test/functional/api/donationsTest.js
+++ test/functional/api/donationsTest.js
@@ -6,6 +6,24 @@ const _ = require("lodash");
 
 let datastore = require("../../../models/donations");
 
-describe('Donations',  () => {
-    // TODO
+describe("Donations", () => {
+    describe("GET /donations", () => {
+        it("should return all the donations", done => {
+            request(server)
+                .get("/donations")
+                .set("Accept", "application/json")
+                .expect("Content-Type", /json/)
+                .expect(200)
+                .end((err, res) => {
+                    expect(res.body).to.be.a("array");
+                    expect(res.body.length).to.equal(2);
+                    const result = _.map(res.body, donation => {
+                        return { id: donation.id, amount: donation.amount };
+                    });
+                    expect(result).to.deep.include({ id: 1000000, amount: 1600 });
+                    expect(result).to.deep.include({ id: 1000001, amount: 1100 });
+                    done(err);
+                });
+        });
+    });
 });
